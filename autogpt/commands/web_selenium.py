ebb0069a73c5d566a2ba41e9d6a369efa9cf9445
Niek van den Bos <niekvandenbos@hotmail.com>
2023-04-19 17:16:32 +0200

    Add arguments to fix browsing.

--- autogpt/commands/web_selenium.py
+++ autogpt/commands/web_selenium.py
@@ -69,6 +69,11 @@ def scrape_text_with_selenium(url: str) -> tuple[WebDriver, str]:
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.49 Safari/537.36"
     )
 
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--remote-debugging-port=9222")
+
     if CFG.selenium_web_browser == "firefox":
         driver = webdriver.Firefox(
             executable_path=GeckoDriverManager().install(), options=options
